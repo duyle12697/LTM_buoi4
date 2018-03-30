7e23fc421ae6396936c2fd47d7566f678d24b3c4
minhduy <jiye1909@gmail.com>
2018-03-30 23:25:37 +0700

    buoi4

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= int.MaxValue; i++)
+            {
+                IPEndPoint ipe = new IPEndPoint(IPAddress.Parse("127.0.0.0"), i);
+                UdpClient server;
+                byte[] data;
+                try
+                {
+                    server = new UdpClient();
+                    server.Client.ReceiveTimeout = 1000;
+                    server.Connect(ipe);
+                    data = new byte[1024];
+                    server.Send(data, data.Length);
+                    data = server.Receive(ref ipe);
+                    Console.WriteLine("{0}: open", ipe);
+                }
+                catch
+                {
+                    Console.WriteLine("{0}: Closed", ipe);
+                }
+            }
+        }
+    }
+}
